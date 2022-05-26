925e375aff051c6c117e2064f798535e73abf8f1
SchegolevOL <92328776+SchegolevOL@users.noreply.github.com>
2022-05-26 23:15:54 +0500

    add PrintPhoneBook

--- PhoneBook.App/Program.cs
+++ PhoneBook.App/Program.cs
@@ -5,6 +5,11 @@
         private static void Main()
         {
             List<PhoneBookItem> phoneBook = new List<PhoneBookItem>();
+
+        }
+        private static void PrintPhoneBook(List<PhoneBookItem>PhoneBook)
+        {
+
         }
     }
 }
