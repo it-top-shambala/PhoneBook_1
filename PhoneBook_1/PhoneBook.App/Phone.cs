914d1ae4d790a0b999b2e15509a6959bac9892aa
Elena-Miro <73115987+Elena-Miro@users.noreply.github.com>
2022-06-06 19:46:59 +0500

    Мироненко Елена
    Программа может только добавлять пользователя, но и не все. Например, не понимаю, как enum использовать, он у меня не работает. 
    Дальше тоже не совсем понимаю, как искать и как удалять.

---
+++ PhoneBook_1/PhoneBook.App/Phone.cs
@@ -0,0 +1,33 @@
+﻿namespace PhoneBook.App
+{
+
+    public class Phone
+    {
+        public Type typeNumber;
+        public string number { get; set; }
+
+        public enum Type
+        {
+            city,
+            mobile
+        }
+        public Phone(Type typeNumber, string number)
+        {
+                number = number;
+
+                typeNumber=typeNumber;
+
+                if (number[0] == '8' || number[1] == '9')
+                {
+                typeNumber = Type.mobile;
+                }
+                else
+                {
+                    typeNumber = Type.city;
+                }
+
+            
+        }
+        
+    }
+}
---
+++ PhoneBook_1/PhoneBook.App/PhoneBookItem.cs
@@ -0,0 +1,32 @@
+﻿
+namespace PhoneBook.App;
+
+
+public  class PhoneBookItem
+{
+    public static string FirstName { get; set; }
+    public static string LastName { get; set; }
+    public static string Patronymic { get; set; }
+    public static List<Phone> Phones { get; set; }
+    public static List<Address> Addresses { get; set; }
+    public static List<string> Groups { get; set; }
+
+
+    public PhoneBookItem()
+    {
+        Phones = new List<Phone>();
+        Addresses = new List<Address>();
+        Groups = new List<string>();
+    }
+    public PhoneBookItem(string lastName, string firstName,  string patronymic, List<Phone> phones,/*Type typeNumber,*/ List<Address> addresses, List<string> groups)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Patronymic = patronymic;
+        Phones = phones;
+        //typeNumber = typeNumber;
+        Addresses = addresses;
+        Groups = groups;
+    }
+   
+}
