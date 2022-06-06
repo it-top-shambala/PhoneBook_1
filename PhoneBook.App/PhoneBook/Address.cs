ef5bbae0052432f69ff241e4b4ca663f4aa81f86
Elena-Miro <73115987+Elena-Miro@users.noreply.github.com>
2022-06-06 19:45:07 +0500

    Мироненко Елена
    Программа может только добавлять пользователя, но и не все. Например, не понимаю, как enum использовать, он у меня не работает. 
    Дальше тоже не совсем понимаю, как искать и как удалять.

---
+++ PhoneBook.App/PhoneBook/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    class Address
+    {
+        public string Type { get; set; }
+        public string AddressBody { get; set; }
+    }
+}
---
+++ PhoneBook.App/PhoneBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhoneBook
+{
+   static class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<PhoneBookItem> phoneBook = new List<PhoneBookItem>();
+        }
+    }
+}
