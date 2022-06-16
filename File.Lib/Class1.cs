7e1177907f0335d0a5ebf1057310fc85d1e9ed62
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-06-17 01:29:23 +0300

    HomeWork

---
+++ File.Lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace File.Lib;
+
+public static class Class1
+{
+    public static void SaveToFile()
+    {
+        
+    }
+}
--- PhoneBook.App/Program.cs
+++ PhoneBook.App/Program.cs
@@ -1,65 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-/*
-Написать программу для реализации "абстрактной" телефонной книги.
-
-    Программа должна уметь:
-
-добавлять данные с клавиатуры
-выводить весь список данных
-удалять данные
-искать по разным полям
-*/
-namespace PhoneBook.App
+﻿namespace PhoneBook.App
 {
-    
+    //TODO Add unit testing to project
     internal static class Program
     {
+        public static readonly string FileName = "test.log";
         private static void Main()
         {
             List<PhoneBookItem> phoneBook = new List<PhoneBookItem>();
-            for (int i = 0; i < 2; i++)
+            string operation;
+            int count_updated = 0;
+            CLI.PrintColord("The end of program file will be saved automatically",ConsoleColor.Blue);
+            Console.WriteLine("Enter operation Add-Remove-Find-Update-Read-Print-Exit");
+            operation = Console.ReadLine();
+            while (true)
             {
-                Console.WriteLine("Enter firstname: ");
-                string? firstname = Console.ReadLine();
-                Console.WriteLine("Enter lastname: ");
-                string? lastname = Console.ReadLine();
-                Console.WriteLine("Enter Patronymic: ");
-                string? patronymic = Console.ReadLine();
-                Console.WriteLine("Enter phone Type: ");
-                string? phoneType = Console.ReadLine();
-                Console.WriteLine("Enter phone Number: ");
-                string? phoneNumber = Console.ReadLine();
-                Phone phone = new Phone() { Type = phoneType, Number = phoneNumber };
-                Console.WriteLine("Enter adress information: ");
-                Address address = new Address() { Type = Console.ReadLine(), AddressBody = Console.ReadLine() };
-                Console.WriteLine("Enter groups: ");
-                string? groups = Console.ReadLine();
-                phoneBook.Add(
-                    new PhoneBookItem(
-                        firstname,
-                        lastname,
-                        patronymic,
-                        phone,
-                        address,
-                        groups
-                    )
-                );
+                if (operation == "exit") break;
+                switch (operation)
+                {
+                    case "Add":
+                    case "add":
+                    {
+                        PhoneBookItem phoneBookItem = CLI.InitalizeItem();
+                        phoneBook.Add(phoneBookItem);
+                        count_updated++;
+                    }break;
+                    case "Remove":
+                    case "remove":
+                    {
+                        string? id = Console.ReadLine();
+                        bool isDone = phoneBook.Remove(new PhoneBookItem(id));
+                        if (isDone)
+                        {
+                            Console.WriteLine("Item succesfully removed!!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"There is not item which is id : {id}");
+                        }
+                    }break;
+                    case "Find":
+                    case "find":
+                    {
+                        Console.WriteLine("Enter value to find object");
+                        string? ValueToFind = Console.ReadLine();
+                        PhoneBookItem findingItem = phoneBook.Find(x => x.IsContain(AttributeEnum.Firstname, ValueToFind));
+                        CLI.PrintToConsolePhoneItem(findingItem);
+                    }break;
+                    case "Update":
+                    case "update":
+                    {
+                        Fs.SaveToFile(FileName,phoneBook);
+                        count_updated = 0;
+                        Console.WriteLine("Updated done!!!");
+                    }break;
+                    case "Read":
+                    case "read":
+                    {
+                        Console.WriteLine("Enter path to read file: ");
+                        string? path = Console.ReadLine();
+                        try
+                        {
+                            if (phoneBook.Count > 0)
+                            {
+                                var list_from_file = Fs.ReadFromFile(path);
+                                phoneBook.AddRange(list_from_file);
+                                count_updated += list_from_file.Count;
+                            }
+                            else
+                            {
+                                phoneBook = Fs.ReadFromFile(path);
+                                count_updated += phoneBook.Count;
+                            }
+                            CLI.PrintColord("Read success",ConsoleColor.Green);
+                        }
+                        catch (FileNotFoundException f)
+                        {
+                            Console.WriteLine($"{f.Message} : {f.FileName}");
+                        }
+                    }break; 
+                    case "Print":
+                    case "print":
+                    {
+                        CLI.PrintToConsoleList(phoneBook);
+                    }break;
+                    default: break;
+                }
+                if (count_updated > 0)
+                {
+                    CLI.PrintColord($"There is {count_updated} update you can update it now Write ---Update--- to console!!",ConsoleColor.Red);
+                }
+                Console.WriteLine("Enter operation again: ");
+                operation = Console.ReadLine();
             }
-            Console.WriteLine();
-            ConsoleWrite.PrintToConsoleList(phoneBook);
-            Console.WriteLine("Enter id to remove: ");
-            string? id = Console.ReadLine();
-            //will be invoked method Equals!
-            phoneBook.Remove(new PhoneBookItem(id));
- 
-            Console.WriteLine("New one: ");
-            ConsoleWrite.PrintToConsoleList(phoneBook);
-            Console.WriteLine("Enter value to find object");
-            string? ValueToFind = Console.ReadLine();
-            PhoneBookItem findingItem = phoneBook.Find(x => x.IsContain(AttributeEnum.Firstname, ValueToFind));
-            Console.WriteLine("Finding item: ");
-            ConsoleWrite.PrintToConsolePhoneItem(findingItem);
+            Fs.SaveToFile(FileName,phoneBook);
         }
     }
 }
